fbe26e37a834d72490f6be23d9789abb3a7adf02
alessandro uleon <alessandrouleon.dev@gmail.com>
2025-12-28 23:06:59 -0400

    inclide route create and list

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -34,16 +34,51 @@ describe('Customer E2E Customer Tests', () => {
         const response = await request(app)
             .post('/customer')
             .send({
-                name: '',
-                // address: {
-                //     street: '123 Main St',
-                //     city: 'Anytown',
-                //     number: 123,
-                //     zip: '12345'
-                // }
+                name: ''
             });
 
         expect(response.status).toBe(500);
+    });
+
+    it('should list all customers', async () => {
+        const response = await request(app)
+            .post('/customer')
+            .send({
+                name: 'John Doe',
+                address: {
+                    street: '123 Main St',
+                    city: 'Anytown',
+                    number: 123,
+                    zip: '12345'
+                }
+            });
+        expect(response.status).toBe(200);
+
+        const response2 = await request(app)
+            .post('/customer')
+            .send({
+                name: 'Jane Smith',
+                address: {
+                    street: '456 Oak St',
+                    city: 'Othertown',
+                    number: 456,
+                    zip: '67890'
+                }
+            });
+        expect(response2.status).toBe(200);
+
+        const listResponse = await request(app).get('/customer').send();
+
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.customers.length).toBe(2);
+
+        const customer = listResponse.body.customers[0];
+        expect(customer.name).toBe('John Doe');
+        expect(customer.address.street).toBe('123 Main St');
+
+        const customer2 = listResponse.body.customers[1];
+        expect(customer2.name).toBe('Jane Smith');
+        expect(customer2.address.street).toBe('456 Oak St');
     });
 
 });
